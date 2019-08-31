82a6d01f5045c3e484865c78ac49363188e869c2
Natsuooo <n17975775@gmail.com>
2019-08-31 11:31:38 +0900

    db

--- main.go
+++ main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+  "log"
+  "net/http"
+  "os"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+  "github.com/gin-gonic/gin"
+  _ "github.com/heroku/x/hmetrics/onload"
+  "database/sql"
+  _ "github.com/lib/pq"
 )
 
+type Test struct{
+  Id int `json:"id"`
+  Content string `json:"content"`
+}
+
 func main() {
-	port := os.Getenv("PORT")
+  port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+  if port == "" {
+      log.Fatal("$PORT must be set")
+  }
+  
+  db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+  if err != nil {
+    log.Fatalf("Error opening database: %q", err)
+  }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+  router := gin.New()
+  router.Use(gin.Logger())
+  router.LoadHTMLGlob("templates/*.tmpl.html")
+  router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+  router.GET("/", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "index.tmpl.html", nil)
+  })
 
-	router.Run(":" + port)
+  router.GET("/test", test)
+  
+  router.GET("/db", dbFunc(db))
+
+  router.Run(":" + port)
+}
+
+func test(c *gin.Context){
+  c.String(http.StatusOK, "test")
+}
+
+func dbFunc(db *sql.DB) gin.HandlerFunc{
+  return func (c *gin.Context){
+    test := Test{}
+    db.QueryRow("SELECT content FROM test").Scan(&test.Content)
+    c.JSON(http.StatusOK, test)
+  }
 }
